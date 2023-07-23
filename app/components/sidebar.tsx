64aa79603d21a07a0eda9f2e2b49333f10878e8e
MatthrewLiu <140250015+MatthrewLiu@users.noreply.github.com>
2023-07-23 20:15:55 +0800

    Update sidebar.tsx
    modify name

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,10 +118,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Welcome to Liu Andong's ChatGPT Exchange Station.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
